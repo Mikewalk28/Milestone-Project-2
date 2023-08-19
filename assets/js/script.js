b7fdd44570e231f5beedb1e3a9eaef0193f0370c
Mikewalk28 <>
2023-08-19 11:40:46 +0000

    Adding questions to the bank and modifying if/else for gameover/score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,38 @@
 // Sample question //
 const quizData = [
-        {
-            question: "Who won the 2023 Challenge Cup Final?",
-            a: "St. Helens",
-            b: "Hull FC",
-            c: "Leigh Leopards",
-            d: "Hull KR",
-            correct: "c",
-        },
+    {
+        question: "Who won the 2023 Challenge Cup Final?",
+        a: "St. Helens",
+        b: "Hull FC",
+        c: "Leigh Leopards",
+        d: "Hull KR",
+        correct: "c",
+    },
+    {
+        question: "Who won the 2023 Challenge Cup Final?",
+        a: "St. Helens",
+        b: "Hull FC",
+        c: "Leigh Leopards",
+        d: "Hull KR",
+        correct: "c",
+    },
+    {
+        question: "Who won the 2023 Challenge Cup Final?",
+        a: "St. Helens",
+        b: "Hull FC",
+        c: "Leigh Leopards",
+        d: "Hull KR",
+        correct: "c",
+    },
 ];
 // Progress questions from typed to API with random question bank //
 const quiz = document.getElementById("quiz");
 const answerElements = document.querySelectorAll(".answer");
 const questionElement = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
+const answerButtonA = document.getElementById("a");
+const answerButtonB = document.getElementById("b");
+const answerButtonC = document.getElementById("c");
+const answerButtonD = document.getElementById("d");
 
 let currentQuiz = 0;
 let score = 0;
@@ -25,38 +41,45 @@ const deselectAnswers = () => {
     answerElements.forEach((answer) => (answer.checked = false));
 };
 
-const getSelected = () => {
-    let answer;
-    answerElements.forEach((answerElement) => {
-        if (answerElement.checked) answer = answerElement.id;
-    });
-    return answer;
-};
 // Quiz to react to user clicks for +1 score/game over upon incorrect //
 const loadQuiz = () => {
     deselectAnswers();
     const currentQuizData = quizData[currentQuiz];
     questionElement.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
+    answerButtonA.innerText = currentQuizData.a;
+    answerButtonB.innerText = currentQuizData.b;
+    answerButtonC.innerText = currentQuizData.c;
+    answerButtonD.innerText = currentQuizData.d;
 };
 
 loadQuiz();
 
-submitButton.addEventListener("click", () => {
-    const answer = getSelected();
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) score++;
-        currentQuiz++;
-        if (currentQuiz < quizData.length) loadQuiz();
-        else {
-            quiz.innerHTML = `
-            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-            <button onclick="history.go(0)">Play Again</button>
-        `;
+const answerButtonList = document.getElementsByClassName('answer');
+for (var i = 0; i < answerButtonList.length; i++) {
+    answerButtonList[i].addEventListener("click", (event) => {
+        const answer = event.target.id;
+        if (answer) {
+            if (answer === quizData[currentQuiz].correct) {
+                score++;
+
+                currentQuiz++;
+                if (currentQuiz < quizData.length) {
+                    loadQuiz();
+                }
+                else {
+                    quiz.innerHTML = `
+                    <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                    <button onclick="history.go(0)">Play Again</button>
+                `;
+                }
+            }
+            else {
+                quiz.innerHTML = `
+                    <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                    <button onclick="history.go(0)">Play Again</button>
+                `;
+            }
         }
-    }
-});
+    });
+}
 
